1bb24f70b7be087ce1e871d1846cb75d0765d22b
Budi Setiawan <budi958585@gmail.com>
2024-02-04 11:46:33 +0700

    Learn variable concept in Golang

---
+++ variables.go
@@ -0,0 +1,44 @@
+package main
+
+// golang variable syntax:
+// var variableName type = value
+// type or value must be specified or both
+// other way:
+// variableName := value
+
+// // declare variable outside the function
+// var a string
+// var b int = 2
+// var c = 3
+
+// // d := 1
+
+func main() {
+	// var student1 string = "Budi Setiawan"
+	// var student2 = "Eka Surya"
+	// number1 := 1
+
+	// fmt.Println(student1)
+	// fmt.Println(student2)
+	// fmt.Println(number1)
+
+	// // Declare variable without value
+	// var a string
+	// var b int
+	// var c bool
+
+	// println(a)
+	// println(b)
+	// println(c)
+
+	// // Value assignment after declaration
+	// var student1 string
+	// student1 = "Budi Setiawan"
+	// println(student1)
+
+	// a = "Budi"
+	// println(a)
+	// println(b)
+	// println(c)
+	// // println(d)
+}
