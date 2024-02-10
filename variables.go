37f163932e92efeed044612b896f277b0db8d2c2
bdstn10 <budistn108@gmail.com>
2024-02-10 15:02:29 +0700

    Switch Case standard usages

--- variables.go
+++ variables.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	if warnaLampu := "hijau"; warnaLampu == "merah" {
-		fmt.Println("Mohon berhenti")
-	} else if warnaLampu == "kuning" {
-		fmt.Println("Mohon Hati-Hati")
-	} else {
-		fmt.Println("Silahkan Jalan")
+	var warnaLampu string = "hijau"
+
+	switch warnaLampu {
+	case "merah":
+		{
+			fmt.Println("Mohon berhenti!")
+		}
+	case "kuning":
+		fmt.Println("Hati-hati")
+		fmt.Println("Jangan nekat")
+	case "hijau":
+		fmt.Println("Silahkan jalan")
+	default:
+		{
+			fmt.Println("Tidak tahu!")
+		}
 	}
 }
