824930571803c542ef33a7d608c93361619caefe
bdstn10 <budistn108@gmail.com>
2024-02-10 14:03:54 +0700

    Trying temporary variable concept

--- variables.go
+++ variables.go
@@ -1,60 +1,13 @@
 package main
 
-// golang variable syntax:
-// var variableName type = value
-// type or value must be specified or both
-// other way:
-// variableName := value
-
-// // declare variable outside the function
-// var a string
-// var b int = 2
-// var c = 3
-
-// // d := 1
+import "fmt"
 
 func main() {
-	// var student1 string = "Budi Setiawan"
-	// var student2 = "Eka Surya"
-	// number1 := 1
-
-	// fmt.Println(student1)
-	// fmt.Println(student2)
-	// fmt.Println(number1)
-
-	// // Declare variable without value
-	// var a string
-	// var b int
-	// var c bool
-
-	// println(a)
-	// println(b)
-	// println(c)
-
-	// // Value assignment after declaration
-	// var student1 string
-	// student1 = "Budi Setiawan"
-	// println(student1)
-
-	// a = "Budi"
-	// println(a)
-	// println(b)
-	// println(c)
-	// // println(d)
-
-	// var a, b, c, d int = 2, 4, 5, 8
-	// var a, b = 6, "Hello"
-	// c, d := 7, "World"
-
-	// var (
-	// 	a int
-	// 	b int    = 6
-	// 	c string = "Hello"
-	// 	d string = "World"
-	// )
-
-	// println(a)
-	// println(b)
-	// println(c)
-	// println(d)
+	if warnaLampu := "hijau"; warnaLampu == "merah" {
+		fmt.Println("Mohon berhenti")
+	} else if warnaLampu == "kuning" {
+		fmt.Println("Mohon Hati-Hati")
+	} else {
+		fmt.Println("Silahkan Jalan")
+	}
 }
