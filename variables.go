519d7e8d08c5783c1062f1319345e05cd11d5318
bdstn10 <budistn108@gmail.com>
2024-02-10 15:36:44 +0700

    Standard 'for loop' in Golang

--- variables.go
+++ variables.go
@@ -3,24 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var warnaLampu string = "kuning"
-
-	// if else like switch case usage
-	switch {
-	case len(warnaLampu) > 5:
-		fmt.Println("Ada cukup banyak karakter di variabel tersebut")
-	case warnaLampu == "merah":
-		{
-			fmt.Println("Mohon berhenti!")
-		}
-	case warnaLampu == "kuning":
-		fmt.Println("Hati-hati")
-		fmt.Println("Jangan nekat")
-	case warnaLampu == "hijau":
-		fmt.Println("Silahkan jalan")
-	default:
-		{
-			fmt.Println("Switch case selesai")
-		}
+	for i := 0; i < 5; i++ {
+		fmt.Println("Berhitung ", i)
 	}
 }
