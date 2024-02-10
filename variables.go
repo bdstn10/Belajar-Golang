69cd043618294ff25b43007a572749fa25f42200
bdstn10 <budistn108@gmail.com>
2024-02-10 16:10:09 +0700

    None limited-size array syntax in golang

--- variables.go
+++ variables.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// Membuat array daftar_siswa dengan ukuran 3 elemen
-	var daftar_siswa [3]string
-	daftar_siswa[0] = "Budi Setiawan"
-	daftar_siswa[1] = "Adifa Kyla"
-	daftar_siswa[2] = "Ramandhika"
+	// Membuat array daftar_siswa dengan ukuran yang tidak dibatasi
+	var daftar_siswa = [...]string{
+		"Budi Setiawan",
+		"Adifa Kyla",
+		"Ramandhika",
+	}
 
 	fmt.Println(daftar_siswa[0], daftar_siswa[1], daftar_siswa[2])
 }
